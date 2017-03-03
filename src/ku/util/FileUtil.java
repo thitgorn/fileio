4fd565aac8cb6691917bb9ee9877347f614f20aa
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2017-03-03 14:29:46 +0700

    first commit

---
+++ src/ku/util/FileUtil.java
@@ -0,0 +1,18 @@
+package ku.util;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileUtil {
+	static void copy(InputStream in, OutputStream out) {
+
+	}
+	
+	static void copy(InputStream in, OutputStream out, int blokcsize){
+		
+	}
+	
+	static void bcopy(InputStream in, OutputStream out){
+		
+	}
+}
