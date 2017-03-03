8be8be451511e424d6e04163ce2306e1384a2b09
Thitiwat Thongbor <thitiwat.tho1@ku.th>
2017-03-03 14:49:38 +0700

    copy as byte

--- src/ku/util/FileUtil.java
+++ src/ku/util/FileUtil.java
@@ -1,18 +1,27 @@
 package ku.util;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class FileUtil {
 	static void copy(InputStream in, OutputStream out) {
+		try {
+			int copyOfIn;
+			while ((copyOfIn = in.read()) < 0) {
+				out.write(copyOfIn);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	static void copy(InputStream in, OutputStream out, int blokcsize) {
 
 	}
-	
-	static void copy(InputStream in, OutputStream out, int blokcsize){
-		
-	}
-	
-	static void bcopy(InputStream in, OutputStream out){
-		
+
+	static void bcopy(InputStream in, OutputStream out) {
+
 	}
 }
